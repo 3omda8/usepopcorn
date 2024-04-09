cf248e0025481c6a324b59e46bfdc8f3d12e6e00
3omda8 <mohameddawood664@gmail.com>
2024-04-09 02:29:20 +0200

    First comment

---
+++ src/useMovies.js
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+
+const KEY = "f84fc31d";
+
+export function useMovies(query) {
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      const controller = new AbortController();
+      async function fetchMovie() {
+        try {
+          setLoading(true);
+          setError("");
+          const res = await fetch(
+            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            { signal: controller.signal }
+          );
+
+          if (!res.ok) throw new Error("Something went wrong");
+
+          const data = await res.json();
+          if (data.Response === "False") throw new Error("Movie not found");
+
+          setMovies(data.Search);
+          setError("");
+        } catch (err) {
+          if (err.name !== "AbortError") {
+            console.log(err.message);
+            setError(err.message);
+          }
+        } finally {
+          setLoading(false);
+        }
+      }
+      if (query.length < 3) {
+        setMovies([]);
+        setError("");
+        return;
+      }
+      //handleCloseMovie();
+      fetchMovie();
+
+      //cleanup function
+      return function () {
+        controller.abort();
+      };
+    },
+    [query]
+  );
+
+  return { movies, loading, error };
+}
